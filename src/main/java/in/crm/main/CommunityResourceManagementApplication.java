ab7334eec35199d8e6cc1531951c693d7418cbc0
[puja hanamghar] <[pujahanamghar@gmail.com]>
2025-01-30 17:37:13 +0530

    Resolved merge conflicts

--- src/main/java/in/crm/main/CommunityResourceManagementApplication.java
+++ src/main/java/in/crm/main/CommunityResourceManagementApplication.java
@@ -9,5 +9,5 @@ public class CommunityResourceManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityResourceManagementApplication.class, args);
 	}
-
+//
 }
